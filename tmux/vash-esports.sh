bc40774dbe2db499ec78e41bd4ec1e67e100190c
Stan Runge <stan@stanrunge.dev>
2024-12-11 13:15:16 +0100

    fix: vash esports project setup

--- tmux/vash-esports.sh
+++ tmux/vash-esports.sh
@@ -5,15 +5,17 @@ if [ $? != 0 ]; then
 
 	tmux new-session -d -s $SESSION_NAME -c ~/dev/vash-esports
 
-	tmux send-keys -t $SESSION_NAME:1 "nvim ." C-m
+	tmux send-keys -t $SESSION_NAME:0 "nvim ." C-m
+
+	tmux new-window -t $SESSION_NAME:1 -c ~/dev/vash-esports/packages/web
+	tmux send-keys -t $SESSION_NAME:1 "sail up -d; sail bun run dev" C-m
 
 	tmux new-window -t $SESSION_NAME:2 -c ~/dev/vash-esports/packages/web
-	tmux send-keys -t $SESSION_NAME:2 "sail up -d; sail bun run dev" C-m
+	tmux send-keys -t $SESSION_NAME:2 "sail up -d; sail artisan queue:work" C-m
 
 	tmux new-window -t $SESSION_NAME:3 -c ~/dev/vash-esports/packages/web
-	tmux send-keys -t $SESSION_NAME:3 "sail artisan queue:work" C-m
-
-	tmux new-window -t $SESSION_NAME:4 -c ~/dev/vash-esports/packages/web
 fi
+
+tmux select-window -t $SESSION_NAME:0
 
 tmux attach -t $SESSION_NAME
